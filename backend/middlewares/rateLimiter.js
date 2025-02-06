1fba65a922f1cfbb861cfec48865d026099fceb7
AdityaDaniel06 <danieladitya327@gmail.com>
2025-02-06 17:55:13 +0530

    handlerFn

--- backend/middlewares/rateLimiter.js
+++ backend/middlewares/rateLimiter.js
@@ -5,5 +5,10 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
   message: "Too many requests, please try again later after 15 minutes.",
+  handler: (req, res, next) => {
+    const clientIp = req.clientIp || req.headers["x-forwarded-for"] || req.ip;
+    console.log(clientIp);
+    res.status(429).json({ error: "Too many requests! Please wait." });
+  },
 });
 module.exports = limiter;
